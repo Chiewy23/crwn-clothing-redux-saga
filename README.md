f77f4a0ef20d0323400a39af303a3bd2a4b818b4
Christopher Hiew <christopherhiew@protonmail.com>
2023-04-13 17:17:20 +0100

    Finished setting up saga.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
  Clothing Retail Website - React JS (with Redux-Saga)
 
 Redux-saga allows developers to perform complex coordination of asynchronous side effects inside of Redux.
+
+Redux-saga replaces thunk.
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,11 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { rootReducer } from './root-reducer';
 import { persistStore, persistReducer } from 'redux-persist';
+import { rootSaga } from './root-saga';
 
 import logger from 'redux-logger';
 import storage from 'redux-persist/lib/storage';
-import thunk from 'redux-thunk';
-
-
-const middleWares = [
-    process.env.NODE_ENV !== "production" && logger,
-    thunk
-].filter(Boolean);
+import createSagaMiddleware from "redux-saga";
 
 const persistConfig = {
     key: "root",        // persist everything
@@ -18,8 +13,18 @@ const persistConfig = {
     whitelist: ["cart"]
 };
 
+const sagaMiddleware = createSagaMiddleware();
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [
+    process.env.NODE_ENV !== "production" && logger,
+    sagaMiddleware
+].filter(Boolean);
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+sagaMiddleware.run(rootSaga);
+
 export const persistor = persistStore(store);
