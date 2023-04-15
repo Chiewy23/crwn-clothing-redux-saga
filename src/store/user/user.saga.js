da2c8b048c28ed638eb51ecd269090f7c049eecc
Christopher Hiew <christopherhiew@protonmail.com>
2023-04-15 08:43:19 +0100

    New generator for obtaining auth snapshot.

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -1,8 +1,18 @@
 import { takeLatest, put, all, call } from "redux-saga/effects";
 import { USER_ACTION_TYPES } from "./user.types";
 import { signInSuccess, signInFailure } from "./user.action";
-import { getCurrentUser } from "../../utilities/firebase/firebase.utils";
+import { getCurrentUser, createUserDocumentFromAuth } from "../../utilities/firebase/firebase.utils";
 
+
+
+export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
+    try {
+        const userSnapshot = yield call(createUserDocumentFromAuth, userAuth, additionalDetails);
+
+    } catch (error) {
+        yield put(signInFailure(error));
+    }
+};
 
 export function* isUserAuthenticated() {
     try {
@@ -11,16 +21,16 @@ export function* isUserAuthenticated() {
             return;
         }
 
-
+        yield call(getSnapshotFromUserAuth, userAuth);
     } catch (error) {
-
+        yield put(signInFailure(error));
     }
 };
 
 export function* onCheckUserSession() {
-    yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, null);
+    yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
 };
 
 export function* userSagas() {
-    yield all([]);
+    yield all([call(onCheckUserSession)]);
 };
