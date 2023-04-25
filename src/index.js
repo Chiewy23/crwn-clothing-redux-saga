8457f9a4dbf6b461aea591134f670fcec8c90b4b
Christopher Hiew <christopherhiew@protonmail.com>
2023-04-25 15:51:49 +0100

    Initial Stripe integration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -16,7 +17,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter >
+        <Elements>
           <App />
+        </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
