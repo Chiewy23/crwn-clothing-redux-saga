65fb5a53e4b8120d7fa748343611d82e445e90e0
Christopher Hiew <christopherhiew@protonmail.com>
2023-04-27 15:40:17 +0100

    Successful Stripe payment test.

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -16,11 +16,37 @@ const PaymentForm = () => {
         if (!stripe || !elements) {
             return;
         }
+
+        const response = await fetch("/.netlify/functions/create-payment-intent", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({ amount: 10000 })
+        }).then(res => res.json());
+
+        const clientSecret = response.paymentIntent.client_secret;
+        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: "Dobby Elf"
+                }
+            }
+        });
+
+        if (paymentResult.error) {
+            alert(paymentResult.error);
+        } else {
+            if (paymentResult.paymentIntent.status === "succeeded") {
+                alert("Payment Successful");
+            }
+        }
     };
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card Payment: </h2>
                 <CardElement />
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
